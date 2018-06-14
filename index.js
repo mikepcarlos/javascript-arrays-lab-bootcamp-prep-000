a5eb478065ccf7dc72248886f0f61d85ad09461f
Michael Carlos <michaelpcarlos@gmail.com>
2018-06-14 18:37:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name);
-  return kittens;
 }
 
 function destructivelyRemoveLastKitten() {
   kittens.pop();
-  return kittens;
 }
 
 function destructivelyRemoveFirstKitten() {
   kittens.shift();
-  return kittens;
 }
 
 function appendKitten(name) {
